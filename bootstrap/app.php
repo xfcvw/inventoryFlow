200691fd575b772d2cc38f1e71c18e6e3212ce4d
Vinicius <vinicius666787@gmail.com>
2026-08-17 17:03:02 -0300

    Configura autenticacao Sanctum da API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     health: '/up',
 )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
-    })
+    $middleware->trustProxies(at: '*');
+    $middleware->statefulApi();
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
